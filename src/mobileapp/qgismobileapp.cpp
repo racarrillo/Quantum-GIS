12aa927c25738d8bfabe5c4c8417511664261701
Marco Bernasocchi <marco@bernawebdesign.ch>
2012-04-04 22:49:29 +0200

    further improvements

--- src/mobileapp/qgismobileapp.cpp
+++ src/mobileapp/qgismobileapp.cpp
@@ -15,7 +15,7 @@
  *                                                                         *
  ***************************************************************************/
 
-#include "qgismobileapp.h"
+#include <qgismobileapp.h>
 #include <QApplication>
 #include <QDeclarativeView>
 #include <QDeclarativeEngine>
@@ -24,24 +24,40 @@
 #include <QMessageBox>
 #include <QKeyEvent>
 #include <QMetaObject>
+#include <QString>
+#include <QApplication>
+#include <QWidget>
 
-#include "qgsapplication.h"
-#include "qgsdataitem.h"
-#include "qgsencodingfiledialog.h"
-#include "qgsgenericprojectionselector.h"
-#include "qgslogger.h"
-#include "qgsmaplayerregistry.h"
-#include "qgsproviderregistry.h"
-#include "qgsvectorlayer.h"
-#include "qgsrasterlayer.h"
-#include "qgsnewvectorlayerdialog.h"
-#include "qgsattributetablemodel.h"
-#include "qgsattributetablefiltermodel.h"
+#include <qgsapplication.h>
+#include <qgsproviderregistry.h>
+#include <qgssinglesymbolrenderer.h>
+#include <qgsmaplayerregistry.h>
+#include <qgsvectorlayer.h>
+#include <qgsmapcanvas.h>
+#include <qgslogger.h>
 
 
 QgisMobileapp::QgisMobileapp( QgsApplication *app, QWidget *parent, Qt::WFlags flags )
     : QMainWindow( parent, flags )
 {
+  // Create the Map Canvas
+  mapCanvas = new QgsMapCanvas();
+  mapCanvas->setObjectName(QString::fromUtf8("mapCanvas"));
+//  mapCanvas->setExtent(mypLayer->extent());
+  mapCanvas->enableAntiAliasing(true);
+  mapCanvas->setCanvasColor( Qt::white );
+
+  addLayer();
+
+  mapCanvas->freeze(false);
+  // Set the Map Canvas Layer Set
+//  mapCanvas->setLayerSet(myLayerSet);
+  mapCanvas->setVisible(true);
+  mapCanvas->refresh();
+
+  mapCanvas->show();
+
+
   mView.setSource(QUrl("qrc:/qml/qgsmobileapp.qml"));
   mView.setResizeMode(QDeclarativeView::SizeRootObjectToView);
 
@@ -49,9 +65,41 @@ QgisMobileapp::QgisMobileapp( QgsApplication *app, QWidget *parent, Qt::WFlags f
 
   mView.setGeometry(100,100, 800, 480);
   mView.show();
+
 }
 
 QgisMobileapp::~QgisMobileapp()
 {
 
+}
+
+void QgisMobileapp::addLayer()
+{
+  QString myLayerPath         = "/tmp";
+  QString myLayerBaseName     = "test";
+  QString myProviderName      = "ogr";
+
+  QgsVectorLayer * mypLayer = new QgsVectorLayer(myLayerPath, myLayerBaseName, myProviderName);
+  QgsSingleSymbolRenderer *mypRenderer = new QgsSingleSymbolRenderer(mypLayer->geometryType());
+  QList<QgsMapCanvasLayer> myLayerSet;
+  mypLayer->setRenderer(mypRenderer);
+
+  if (mypLayer->isValid())
+  {
+    qDebug("Layer is valid");
+  }
+  else
+  {
+    qDebug("Layer is NOT valid");
+    return;
+  }
+  // Add the Vector Layer to the Layer Registry
+  QgsMapLayerRegistry::instance()->addMapLayer(mypLayer, TRUE);
+
+  // Add the Layer to the Layer Set
+  myLayerSet.append(QgsMapCanvasLayer(mypLayer));
+  // set the canvas to the extent of our layer
+  mapCanvas->setExtent(mypLayer->extent());
+  // Set the Map Canvas Layer Set
+  mapCanvas->setLayerSet(myLayerSet);
 }
